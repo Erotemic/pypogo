7ba6dea9a3ff9e88aadfb44a51cdb1e9bf16662b
joncrall <jon.crall@kitware.com>
2022-07-12 01:18:44 -0400

    wip

--- pypogo/utils/__init__.py
+++ pypogo/utils/__init__.py
@@ -3,15 +3,9 @@ mkinit ~/code/pypogo/pypogo/utils/__init__.py --lazy -w
 """
 
 
-
-
 def lazy_import(module_name, submodules, submod_attrs):
-    import sys
     import importlib
-    import importlib.util
-    all_funcs = []
-    for mod, funcs in submod_attrs.items():
-        all_funcs.extend(funcs)
+    import os
     name_to_submod = {
         func: mod for mod, funcs in submod_attrs.items()
         for func in funcs
@@ -20,14 +14,14 @@ def lazy_import(module_name, submodules, submod_attrs):
     def __getattr__(name):
         if name in submodules:
             attr = importlib.import_module(
-                '{module_name}.{modname}'.format(
-                    module_name=module_name, modname=modname)
+                '{module_name}.{name}'.format(
+                    module_name=module_name, name=name)
             )
         elif name in name_to_submod:
-            modname = name_to_submod[name]
+            submodname = name_to_submod[name]
             module = importlib.import_module(
-                '{module_name}.{modname}'.format(
-                    module_name=module_name, modname=modname)
+                '{module_name}.{submodname}'.format(
+                    module_name=module_name, submodname=submodname)
             )
             attr = getattr(module, name)
         else:
@@ -36,14 +30,22 @@ def lazy_import(module_name, submodules, submod_attrs):
                     module_name=module_name, name=name))
         globals()[name] = attr
         return attr
+
+    if os.environ.get('EAGER_IMPORT', ''):
+        for name in name_to_submod.values():
+            __getattr__(name)
+
+        for attrs in submod_attrs.values():
+            for attr in attrs:
+                __getattr__(attr)
     return __getattr__
 
 
 __getattr__ = lazy_import(
     __name__,
-    submodules=[
+    submodules={
         'priority_queue',
-    ],
+    },
     submod_attrs={
         'priority_queue': [
             'PriorityData',
